9f0e48effc343a39186181a90ea979e74ef9cd82
Jayson Yan <jj2yan@uwaterloo.ca>
2018-09-29 15:46:32 -0400

    added upload system

--- myapp/index.js
+++ myapp/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const upload  = require("express-fileupload");
 app.use(express.static(__dirname));
+app.use(upload());
 
 app.get('/', (req, res) => {
   // res.send('Hello World!');
@@ -11,4 +12,23 @@ app.get('/', (req, res) => {
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!');
+});
+
+app.post('/upload', function(req, res) {
+  if (req.files) {
+    console.log(req.files)
+    var file = req.files.fileToUpload,
+      name = "image.png";
+    var uploadpath = __dirname + '/uploads/' + name;
+    file.mv(uploadpath,function(err) {
+      if(err) {
+        console.log(err);
+        res.send("error occured");
+      }
+      else {
+        res.send("Done!");
+      }
+    });
+
+  }
 });
