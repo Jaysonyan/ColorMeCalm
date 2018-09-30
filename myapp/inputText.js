7df024176e1bf462d625c91ae977cd600875d94b
Yu Heng Su <yuheng.su@mail.utoronto.ca>
2018-09-30 05:12:48 -0400

    Disabled submitted button until picture has been uploaded

--- myapp/inputText.js
+++ myapp/inputText.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    $('form input').change(function () {
-    
-      $('#select').text("Uploading...");  
-      setTimeout(function(){$('#select').text("Upload successful.");},1000);
-    });
-
-    let smit = document.getElementById('uploadImg');
-    smit.addEventListener('click',sub, false);
-
-    function sub(){
-      $('#select').text("Submitted.");
-    }
-
+  $('form input').change(function () {
+    $('#select').text("Uploading...");  
+    setTimeout(function(){$('#select').text("Upload successful.");},1000);
+    setTimeout(function(){$(':input[type="submit"]').removeAttr("disabled");},1000);
   });
+
+  let smit = document.getElementById('uploadImg');
+  smit.addEventListener('click',sub, false);
+
+  function sub(){
+    $('#select').text("Submitted.");
+  }
+
+});
